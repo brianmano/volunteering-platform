3fdb4e773139bfdf227482f98f3291129a925dba
Brian Manomaisupat <brian.manomaisupat@gmail.com>
2025-12-01 01:10:48 -0500

    kudos

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'VolunteerHub',
-  description: 'Enjoy',
+  title: 'VolunteerIn',
+  description: '#kudos',
   generator: 'v0.app',
   icons: {
     icon: '/icon.svg',
